c4e87555c7ab379c0beb467f4bad203661606cc1
Abhilash <abhilash@Abhilashs-Mac.local>
2018-10-24 05:14:38 -0700

    First commit

---
+++ iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Abhilash on 24/10/18.
+//  Copyright © 2018 SweetGodzillas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
